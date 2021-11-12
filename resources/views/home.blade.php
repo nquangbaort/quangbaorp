a26b8ca4efc5ad336dadbcd700188b134c46cd5e
baonq <baonq@vaixgroup.com>
2021-11-12 17:03:17 +0700

    Push

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <title>Document</title>
+</head>
+<body>
+<canvas id='canvas1'></canvas>
+
+<footer>Created by <a href="https://fb.com/quangbaond" target='blank'>Quang Bao RP</a> @2020.
+</footer>
+<script type="text/javascript" src="{{ asset('assets/js/index.js')}}"></script>
+</body>
+</html>
